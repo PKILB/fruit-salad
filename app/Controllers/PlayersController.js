4626e4e1379124d16f38b09349a232074d05f726
Payton Boone <paytonboone30@gmail.com>
2023-02-02 14:42:04 -0700

    High Score Template

--- app/Controllers/PlayersController.js
+++ app/Controllers/PlayersController.js
@@ -1,9 +1,11 @@
+import { appState } from "../AppState.js"
 
 
 
 export class PlayersController {
 constructor(){
     console.log('We are working')
+    console.log(appState.players)
 }
 
 }
--- app/Models/Player.js
+++ app/Models/Player.js
@@ -1,6 +1,11 @@
+import { generateId } from "../Utils/generateId.js"
 
 
 
 export class Player {
-  
+    constructor(data) {
+        this.id = data.id || generateId()
+        this.name = data.name
+        this.score = data.score || 0
+    }
 }
